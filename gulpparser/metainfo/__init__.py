8b276021147c651a94fc8f4925a395e4531d5f75
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2021-08-27 01:15:02 +0200

    Adapted further metainfo changes

--- gulpparser/metainfo/__init__.py
+++ gulpparser/metainfo/__init__.py
@@ -16,4 +16,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import gulpparser.metainfo.gulp
+from nomad.metainfo import Environment
+
+from . import gulp
+
+m_env = Environment()
+m_env.m_add_sub_section(Environment.packages, gulp.m_package)
--- gulpparser/metainfo/gulp.py
+++ gulpparser/metainfo/gulp.py
@@ -25,6 +25,9 @@ from nomad.metainfo import (  # pylint: disable=unused-import
 from nomad.datamodel.metainfo import run
 
 
+m_package = Package()
+
+
 class x_gulp_section_main_keyword(MSection):
     '''
     Section for GULP calculation mode input variable
