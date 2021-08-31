5f26ea3cd376a8e334c59b461ecac8b13f691f34
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2021-08-31 14:23:17 +0200

    Adapted further changes in metainfo

--- gulpparser/metainfo/gulp.py
+++ gulpparser/metainfo/gulp.py
@@ -22,7 +22,7 @@ from nomad.metainfo import (  # pylint: disable=unused-import
     MSection, MCategory, Category, Package, Quantity, Section, SubSection, SectionProxy,
     Reference
 )
-from nomad.datamodel.metainfo import run
+from nomad.datamodel.metainfo import simulation
 
 
 m_package = Package()
@@ -212,7 +212,7 @@ class x_gulp_section_forcefield(MSection):
         ''')
 
 
-class System(run.system.System):
+class System(simulation.system.System):
 
     m_def = Section(validate=False, extends_base_section=True)
 
@@ -329,7 +329,7 @@ class System(run.system.System):
         ''')
 
 
-class Run(run.run.Run):
+class Run(simulation.run.Run):
 
     m_def = Section(validate=False, extends_base_section=True)
 
@@ -345,7 +345,7 @@ class Run(run.run.Run):
         repeats=True)
 
 
-class Method(run.method.Method):
+class Method(simulation.method.Method):
 
     m_def = Section(validate=False, extends_base_section=True)
 
@@ -368,7 +368,7 @@ class Method(run.method.Method):
         repeats=True)
 
 
-class Calculation(run.calculation.Calculation):
+class Calculation(simulation.calculation.Calculation):
 
     m_def = Section(validate=False, extends_base_section=True)
 
